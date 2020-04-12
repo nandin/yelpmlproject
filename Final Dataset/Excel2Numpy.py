287bc64e1c883411b79808397a49e978c0f2e6b8
Prithvi Rathaur <rathaur.prithvi@gmail.com>
2020-04-12 19:51:53 -0400

    changed dataset for stars label to be stars * 2 so things could all be integers

--- Final Dataset/Excel2Numpy.py
+++ Final Dataset/Excel2Numpy.py
@@ -3,4 +3,5 @@ import numpy as np
 
 restaurants = pd.read_excel('FinalDataSet.xlsx')
 restaurants = restaurants.to_numpy()
+restaurants[:,-1] = restaurants[:, -1] * 2
 np.save('FinalDataSet.npy', restaurants)
--- Random Forest/yelpRandomForest.py
+++ Random Forest/yelpRandomForest.py
@@ -3,22 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # load the dataset
-data = np.load('dataset.npy')
+data = np.load('FinalDataSet.npy')
 data = data.astype('int')
 numDataPoints = np.size(data, 0)
 
 # Split data set into training and testing -> 80% training 20% testing
-eightyPerc = int(np.around(numDataPoints * 0.8))
-trainingData = data[:eightyPerc, 1:10]
-testingData = data[eightyPerc:, 1:10]
+Perc = int(np.around(numDataPoints * 0.7))
+trainingData = data[:Perc, 1:10]
+testingData = data[Perc:, 1:10]
 
 # Split labels into training and testing
-trainingLabels = np.around(data[:eightyPerc, 10])
-testingLabels = np.around(data[eightyPerc:, 10])
+trainingLabels = np.around(data[:Perc, 10])
+testingLabels = np.around(data[Perc:, 10])
 
 # see effect of number of trees in random forest on data
-accuracy = np.zeros(1000)
-for i in range(1000):
+accuracy = np.zeros(100)
+for i in range(100):
 	model = RandomForestClassifier(n_estimators = i+1, 
 									bootstrap = True, 
 									criterion = 'entropy', 
@@ -26,7 +26,7 @@ for i in range(1000):
 	model.fit(trainingData, trainingLabels)
 	accuracy[i] = model.score(testingData, testingLabels)
 
-numberTrees = np.arange(1, 1001, 1)
+numberTrees = np.arange(1, 101, 1)
 plt.scatter(numberTrees, accuracy)
 plt.show()
 
